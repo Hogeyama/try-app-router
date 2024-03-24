5312b340684ffcb228663624ea754b4375e96d1f
Hogeyama <gan13027830@gmail.com>
2024-03-25 02:35:32 +0900

    feat: Correct or ignore lint issues

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
+// biome-ignore lint/suspicious/noRedeclare: This is best practice!
 export const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
